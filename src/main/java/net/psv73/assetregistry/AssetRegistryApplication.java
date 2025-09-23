cc80fd7011de9a5631ed40a004dd4b54015992ff
psv73 <psv73.mail@gmail.com>
2025-09-23 10:05:45 +0200

    chore: bootstrap Spring Boot skeleton (Web, Security, JPA, Validation, Lombok, Flyway, PostgreSQL)

---
+++ src/main/java/net/psv73/assetregistry/AssetRegistryApplication.java
@@ -0,0 +1,13 @@
+package net.psv73.assetregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssetRegistryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/psv73/assetregistry/AssetRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package net.psv73.assetregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
